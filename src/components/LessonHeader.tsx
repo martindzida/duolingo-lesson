54c8293a9f340fc58af484bc91673699d369da64
martindzida <martin.d14@email.cz>
2022-11-26 12:46:32 +0100

    current streak, styling

--- src/components/LessonHeader.tsx
+++ src/components/LessonHeader.tsx
@@ -1,17 +1,21 @@
 import LeaveLessonBtn from './LeaveLessonBtn';
 import ProgressBar from './ProgressBar';
+import StreakInfo from './StreakInfo';
 
 interface Props {
   progress: number;
   handleLeaveLesson: () => void;
 }
 
-const LessonHeader = ({}: Props) => {
+const LessonHeader = ({ progress }: Props) => {
   return (
-    <div className='flex justify-center items-center w-full gap-8'>
-      <LeaveLessonBtn />
-      <ProgressBar progress={10} />
-    </div>
+    <>
+      <div className='flex justify-center items-center w-full gap-8'>
+        <LeaveLessonBtn />
+        <ProgressBar progress={progress} />
+      </div>
+      <StreakInfo streak={5} />
+    </>
   );
 };
 
--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -3,9 +3,17 @@ interface Props {
 }
 
 const ProgressBar = ({ progress }: Props) => {
+  const mediumThr = 30;
+  const highThr = 75;
+  let barColor = 'bg-green-400';
+
+  if (progress > 33) {
+    barColor = progress > 75 ? 'bg-orange-500' : 'bg-amber-400';
+  }
   return (
-    <div className='relative bg-slate-200 w-1/2 h-4 rounded-full'>
-      <div className='absolute bg-green-400 h-4 rounded-full' style={{ width: `${progress}%` }}></div>
+    <div className='relative bg-slate-200 w-1/2 h-4 rounded-full shadow-lg'>
+      <div className={`absolute ${barColor} h-4 rounded-full`} style={{ width: `${progress}%` }}></div>
+      <div className='absolute bg-slate-100 opacity-30 h-1 rounded-full left-2 top-1' style={{ width: `${progress - 2}%` }}></div>
     </div>
   );
 };
