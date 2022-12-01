ec3750ff24c627a09f688b7e87a0dc8270ed9bdf
martindzida <martin.d14@email.cz>
2022-12-01 22:11:38 +0100

    useQuery, validating answer

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import { useEffect, useState } from 'react';
-import WordTile from './components/WordTile';
-import LessonHeader from './components/LessonHeader';
-import CheckBtn from './components/CheckBtn';
-import CheckResponse from './components/CheckResponse';
-import useShuffleArray from './utils/useShuffleArray';
-import useParseSentence from './utils/useParseSente';
-import useGetProgress from './utils/useGetProgress';
+import { useEffect, useState } from 'react'
+import WordTile from './components/WordTile'
+import LessonHeader from './components/LessonHeader'
+import CheckBtn from './components/CheckBtn'
+import CheckResponse from './components/CheckResponse'
+import getProgress from './utils/getProgress'
+import getWords from './utils/getWords'
+import { useQuery } from '@tanstack/react-query'
 
-export type wordObj = { id: number; word: string };
+export type WordObj = { id: number; word: string }
 
 function App() {
   //TODO: find font on Google Fonts
@@ -16,67 +16,82 @@ function App() {
     'Duo wants to know your location',
     'How many lessons have you done today',
     'One lesson a day keeps the doctors away',
-  ];
+  ]
 
-  const [curSenId, setCurSenId] = useState(0);
-  const [isValid, setIsValid] = useState<boolean | null>(null);
-  const [isCompleted, setIsCompleted] = useState(false);
-  const [words, setWords] = useState<wordObj[]>(useShuffleArray(useParseSentence(senteces[curSenId])));
-  const [picked, setPicked] = useState<wordObj[]>([]);
-  const [streak, setStreak] = useState(0);
-  const [canContinue, setCanContinue] = useState(false);
+  const [curSenId, setCurSenId] = useState(0)
+  const [isCorrect, setIsCorrect] = useState<boolean | null>(null)
+  const [isCompleted, setIsCompleted] = useState(false)
+  const [words, setWords] = useState<WordObj[]>([])
+  const [picked, setPicked] = useState<WordObj[]>([])
+  const [streak, setStreak] = useState(0)
+  const [canContinue, setCanContinue] = useState(false)
+  const [correctAnswersCount, setCorrectAnswersCount] = useState(0)
+  const [wrongAnswers, setWrongAnswers] = useState<WordObj[]>([])
 
   useEffect(() => {
-    setWords(useShuffleArray(useParseSentence(senteces[curSenId])));
-    setIsValid(null);
-    setCanContinue(false);
-  }, [canContinue]);
+    setWords(getWords(senteces[curSenId]))
+    setIsCorrect(null)
+    setCanContinue(false)
+    setPicked([])
+  }, [canContinue])
 
-  const checkWordOrder = (words: wordObj[]) => {
-    const convStr = words.map((w: wordObj) => w.word).join(' ');
-    const valid = convStr === senteces[curSenId];
-    setIsValid(valid);
+  const fetchQuotes = async () => {
+    const res = await fetch('https://api.quotable.io/quotes')
+    return res.json()
+  }
+  const { data, isLoading } = useQuery(['getQuotes'], fetchQuotes)
+
+  if (isLoading) {
+    return <div>Loading...</div>
+  }
+
+  const checkWordOrder = (words: WordObj[]) => {
+    const convStr = words.map((w: WordObj) => w.word).join(' ')
+    const correct = convStr === senteces[curSenId]
     //TODO: think of the end of the lesson
-    if (valid) {
+    if (correct) {
+      setCorrectAnswersCount(prev => prev + 1)
       if (curSenId < senteces.length - 1) {
-        setCurSenId(prev => prev + 1);
-        setStreak(prev => prev + 1);
+        setStreak(prev => prev + 1)
       } else {
-        setIsCompleted(true);
+        setIsCompleted(true)
       }
-
-      setPicked([]);
-    }
-  };
-
-  const handleTileClick = (tile: wordObj) => {
-    if (picked.includes(tile)) {
-      setPicked(picked.filter((w: wordObj) => w.id !== tile.id));
     } else {
-      setPicked(prev => [...prev, tile]);
+      setWrongAnswers(getWords(senteces[curSenId]))
+      setStreak(0)
     }
-  };
+    setIsCorrect(correct)
+    setCurSenId(prev => prev + 1)
+  }
 
-  const handleLeaveLesson = () => {};
+  const handleTileClick = (tile: WordObj) => {
+    if (picked.includes(tile)) {
+      setPicked(picked.filter((word: WordObj) => word.id !== tile.id))
+    } else {
+      setPicked(prev => [...prev, tile])
+    }
+  }
+
+  const handleLeaveLesson = () => {}
 
   return (
     <div className='w-screen min-h-screen flex flex-col items-center justify-center gap-16'>
-      <LessonHeader progress={useGetProgress(curSenId, senteces.length)} streak={streak} handleLeaveLesson={handleLeaveLesson} />
+      <LessonHeader progress={getProgress(correctAnswersCount, senteces.length)} streak={streak} handleLeaveLesson={handleLeaveLesson} />
       <div className='w-1/2 h-32 flex justify-center gap-5 items-end border-b-2 border-slate-500 p-2 border-opacity-60'>
-        {picked.map((w: wordObj) => (
+        {picked.map((w: WordObj) => (
           <WordTile key={w.id} word={w} isPicked={false} handleTileClick={handleTileClick} />
         ))}
       </div>
       <div className='flex justify-center gap-5'>
-        {words.map((w: wordObj) => (
+        {words.map((w: WordObj) => (
           <WordTile key={w.id} word={w} isPicked={picked.includes(w)} handleTileClick={handleTileClick} />
         ))}
       </div>
       <CheckBtn isDisabled={picked.length === 0} handleCheck={() => checkWordOrder(picked)} />
       <div>{isCompleted && 'Congratulations!'}</div>
-      {isValid !== null && <CheckResponse isCorrect={isValid} correctAnswer={senteces[curSenId]} handleContinue={() => setCanContinue(true)} />}
+      {isCorrect !== null && <CheckResponse isCorrect={isCorrect} correctAnswer={senteces[curSenId]} handleContinue={() => setCanContinue(true)} />}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
