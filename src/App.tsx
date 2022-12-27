0d19160e8ba0b08cc634ffecbf84d32f06c56410
martindzida <martin.d14@email.cz>
2022-12-27 21:57:07 +0100

    logic, getting rid of useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,67 @@
-import { useEffect, useState } from 'react'
-import WordTile from './components/WordTile'
+import { useState } from 'react'
+import { useQuery } from '@tanstack/react-query'
+import fetchQuotes from './utils/fetchQuotes'
+import getWords from './utils/getWords'
+import getProgress from './utils/getProgress'
 import LessonHeader from './components/LessonHeader'
 import CheckBtn from './components/CheckBtn'
 import CheckResponse from './components/CheckResponse'
-import getProgress from './utils/getProgress'
-import getWords from './utils/getWords'
-import fetchQuotes from './utils/fetchQuotes'
-import { useQuery } from '@tanstack/react-query'
 import StartPage from './components/StartPage'
 import PickedTilesList from './components/PickedTilesList'
 import WordStackList from './components/WordStackList'
 
 export type WordObj = { id: number; word: string }
+type LessonStageType = 'initial' | 'errors' | 'completed'
 
 function App() {
   //TODO: find font on Google Fonts
-  const senteces: string[] = ['Duo calls for a lesson', 'Duo wants to know your location']
+  const senteces: string[] = [
+    'Duo calls for a lesson',
+    'Duo wants to know your location',
+  ]
 
-  const [curSenId, setCurSenId] = useState(0)
+  const [curId, setCurId] = useState(0)
   const [isCorrect, setIsCorrect] = useState<boolean | null>(null)
-  const [isCompleted, setIsCompleted] = useState(false)
-  const [words, setWords] = useState<WordObj[]>([])
+  const [lessonStage, setLessonStage] = useState<LessonStageType>('initial')
+  const [words, setWords] = useState<WordObj[]>(getWords(senteces[curId]))
   const [picked, setPicked] = useState<WordObj[]>([])
   const [streak, setStreak] = useState(0)
   const [canContinue, setCanContinue] = useState(false)
   const [correctAnswersCount, setCorrectAnswersCount] = useState(0)
-  const [wrongAnswers, setWrongAnswers] = useState<WordObj[]>([])
+  const [wrongAnswers, setWrongAnswers] = useState<number[]>([])
   const [isOngoing, setIsOngoing] = useState(true)
 
-  useEffect(() => {
-    setWords(getWords(senteces[curSenId]))
-    setIsCorrect(null)
-    setCanContinue(false)
-    setPicked([])
-  }, [canContinue])
-
   const { data, isLoading } = useQuery(['getQuotes'], fetchQuotes)
-  console.log(data)
+
   if (isLoading) {
     return <div>Loading...</div>
+  }
+
+  const quotesWords = data.results.map((quote: any) => getWords(quote.content))
+
+  if (canContinue) {
+    setWords(quotesWords[curId])
+    setIsCorrect(null)
+    setPicked([])
+    //TODO: will be null, probably
+    setCanContinue(false)
   }
 
   //FIXME: cursed
   const checkWordOrder = (words: WordObj[]) => {
     const convStr = words.map((w: WordObj) => w.word).join(' ')
-    const correct = convStr === senteces[curSenId]
+    const correct = convStr === senteces[curId]
+    setCanContinue(false)
+    setIsCorrect(correct)
+
     if (correct) {
-      setIsCorrect(correct)
       setStreak(prev => prev + 1)
-      if (curSenId < senteces.length - 1) {
+      if (correctAnswersCount < senteces.length - 1) {
+        setCurId(prev => prev + 1)
         setCorrectAnswersCount(prev => prev + 1)
-        setCurSenId(prev => prev + 1)
-      } else {
-        setIsCompleted(true)
       }
     } else {
-      setWrongAnswers(getWords(senteces[curSenId]))
+      setWrongAnswers(prev => [...prev, curId])
       setStreak(0)
     }
   }
@@ -68,24 +74,32 @@ function App() {
     }
   }
 
-  const handleLeaveLesson = () => {
-    setIsOngoing(false)
-  }
-
   return (
     <div className='w-screen min-h-screen flex flex-col items-center justify-center gap-16'>
       {!isOngoing && <StartPage />}
-      <LessonHeader progress={getProgress(correctAnswersCount, senteces.length)} streak={streak} handleLeaveLesson={handleLeaveLesson} />
+      <LessonHeader
+        progress={getProgress(correctAnswersCount, senteces.length)}
+        streak={streak}
+        handleLeaveLesson={() => setIsOngoing(false)}
+      />
       <PickedTilesList picked={picked} handleTileClick={handleTileClick} />
-      <WordStackList words={words} picked={picked} handleTileClick={handleTileClick} />
-      <div className='flex justify-center gap-5'>
-        {words.map((w: WordObj) => (
-          <WordTile key={w.id} word={w} isPicked={picked.includes(w)} handleTileClick={handleTileClick} />
-        ))}
-      </div>
-      <CheckBtn isDisabled={picked.length === 0} handleCheck={() => checkWordOrder(picked)} />
-      <div>{isCompleted && 'Congratulations!'}</div>
-      {isCorrect !== null && <CheckResponse isCorrect={isCorrect} correctAnswer={senteces[curSenId]} handleContinue={() => setCanContinue(true)} />}
+      <WordStackList
+        words={words}
+        picked={picked}
+        handleTileClick={handleTileClick}
+      />
+      <CheckBtn
+        isDisabled={!picked.length || isCorrect !== null}
+        handleCheck={() => checkWordOrder(picked)}
+      />
+      <div>{lessonStage === 'completed' && 'Congratulations!'}</div>
+      {isCorrect !== null && (
+        <CheckResponse
+          isCorrect={isCorrect}
+          correctAnswer={senteces[curId]}
+          handleContinue={() => setCanContinue(true)}
+        />
+      )}
     </div>
   )
 }
