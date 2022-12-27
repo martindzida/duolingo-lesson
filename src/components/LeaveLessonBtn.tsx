d3b353f3d64cb1397e73359282dfd0edf025a095
martindzida <martin.d14@email.cz>
2022-12-27 22:32:16 +0100

    leaving lesson handling

--- src/components/LeaveLessonBtn.tsx
+++ src/components/LeaveLessonBtn.tsx
@@ -1,14 +1,32 @@
-const LeaveLessonBtn = () => {
+import { Dispatch, SetStateAction } from 'react'
+import { LessonStageType } from '../App'
+
+interface Props {
+  handleLeave: Dispatch<SetStateAction<LessonStageType>>
+}
+
+const LeaveLessonBtn = ({ handleLeave }: Props) => {
   return (
     <button
-      onClick={() => console.log('cancle lesson')}
+      onClick={() => handleLeave('start')}
       className='text-slate-500 transition duration-200 ease-in-out hover:bg-slate-100 rounded-full p-1'
     >
-      <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' strokeWidth={1.5} stroke='currentColor' className='w-8 h-8'>
-        <path strokeLinecap='round' strokeLinejoin='round' d='M6 18L18 6M6 6l12 12' />
+      <svg
+        xmlns='http://www.w3.org/2000/svg'
+        fill='none'
+        viewBox='0 0 24 24'
+        strokeWidth={1.5}
+        stroke='currentColor'
+        className='w-8 h-8'
+      >
+        <path
+          strokeLinecap='round'
+          strokeLinejoin='round'
+          d='M6 18L18 6M6 6l12 12'
+        />
       </svg>
     </button>
-  );
-};
+  )
+}
 
-export default LeaveLessonBtn;
+export default LeaveLessonBtn
--- src/components/LessonHeader.tsx
+++ src/components/LessonHeader.tsx
@@ -1,23 +1,25 @@
-import LeaveLessonBtn from './LeaveLessonBtn';
-import ProgressBar from './ProgressBar';
-import StreakInfo from './StreakInfo';
+import LeaveLessonBtn from './LeaveLessonBtn'
+import ProgressBar from './ProgressBar'
+import StreakInfo from './StreakInfo'
+import { Dispatch, SetStateAction } from 'react'
+import { LessonStageType } from '../App'
 
 interface Props {
-  progress: number;
-  streak: number;
-  handleLeaveLesson: () => void;
+  progress: number
+  streak: number
+  handleLeaveLesson: Dispatch<SetStateAction<LessonStageType>>
 }
 
 const LessonHeader = ({ progress, streak, handleLeaveLesson }: Props) => {
   return (
     <>
       <div className='flex justify-center items-center w-full gap-8'>
-        <LeaveLessonBtn />
+        <LeaveLessonBtn handleLeave={handleLeaveLesson} />
         <ProgressBar progress={progress} />
       </div>
       <StreakInfo streak={streak} />
     </>
-  );
-};
+  )
+}
 
-export default LessonHeader;
+export default LessonHeader
