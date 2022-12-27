6cd6aa9798cfa2519952be7e5ef79305ceebf066
martindzida <martin.d14@email.cz>
2022-12-27 22:31:12 +0100

    start page

--- src/components/StartPage.tsx
+++ src/components/StartPage.tsx
@@ -1,5 +1,21 @@
-const StartPage = () => {
-  return <div>StartPage</div>;
-};
+import { Dispatch, SetStateAction } from 'react'
+import { LessonStageType } from '../App'
 
-export default StartPage;
+interface Props {
+  handleStart: () => void
+}
+
+const StartPage = ({ handleStart }: Props) => {
+  return (
+    <div>
+      <button
+        className='bg-green-500 text-white transition ease-in-out duration-200 hover:bg-green-600 text-base font-extrabold uppercase rounded-lg px-6 py-3'
+        onClick={handleStart}
+      >
+        Start lesson
+      </button>
+    </div>
+  )
+}
+
+export default StartPage
