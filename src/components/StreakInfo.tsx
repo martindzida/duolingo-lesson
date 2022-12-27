382435fe8bcdb20787f2149cd5c9177e482793c8
martindzida <martin.d14@email.cz>
2022-12-27 21:53:04 +0100

    shadow
    shadow

--- src/components/StreakInfo.tsx
+++ src/components/StreakInfo.tsx
@@ -6,10 +6,9 @@ interface Props {
 
 const StreakInfo = ({ streak }: Props) => {
   return (
-    <div className='flex justify-center items-center text-orange-500 text-base font-extrabold uppercase'>
+    <div className='flex justify-center items-center w-20 h-20 text-orange-500 bg-white rounded-full text-base font-extrabold uppercase p-2 shadow-orange-300 shadow-streak'>
       <FireIcon className='w-8 h-8 mr-2' />
       <span className='text-3xl'>{streak}</span>
-      <span className='p-2'>streak</span>
     </div>
   )
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,11 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        streak: '0 0 50px 15px rgba(0, 0, 0, 0.3)',
+      },
+    },
   },
   plugins: [],
-};
+}
